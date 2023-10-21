8fd5ce000582ca462038aba78527f2cfd4ae6192
Levente Török <tlevi0409@gmail.com>
2023-10-21 11:26:54 +0200

    Megirtam a programot

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 import './style.css'
 
+document.addEventListener('DOMContentLoaded',()=>{
+    
+    document.getElementById('url').addEventListener('input',()=>{
+        document.getElementById('kep').src = document.getElementById('url').value;
+    })
+
+    document.getElementById('textSize').addEventListener('input',()=>{
+        document.getElementById('text').style.fontSize =document.getElementById('textSize').value + "pt";
+    })
+
+    document.getElementById('sorkoz').addEventListener('input',()=>{
+        document.getElementById('text').style.lineHeight = document.getElementById('sorkoz').value + "em";
+    })
+
+    document.getElementById('color').addEventListener('input',()=>{
+        document.getElementById('text').style.color=document.getElementById('color').value;
+    })
+
+    document.getElementById('button').addEventListener('click',()=>{
+        document.getElementById('kep').classList.toggle("right");
+    })
+
+});
+
+
+
+
